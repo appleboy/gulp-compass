cc0e3e80713d0ce00f3e518e5791a7f7cdd47d8b
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-12-25 00:24:31 +0800

    initial index.js
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ index.js
@@ -0,0 +1,20 @@
+var es = require('event-stream'),
+    gutil = require('gulp-util'),
+    Buffer = require('buffer').Buffer;
+
+module.exports = function(opt){
+    function compile(file, cb){
+        try {
+            //file.contents = new Buffer(coffee.compile(String(file.contents), opt));
+            file.contents = new Buffer(String(file.contents));
+        } catch (err) {
+            //var newError = formatError(file, err);
+            //return cb(newError);
+        }
+        file.path = gutil.replaceExtension(file.path, ".css");
+        //console.log(file.path);
+        cb(null, file);
+    }
+
+    return es.map(compile);
+};
