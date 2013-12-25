b500c439c7259c4bb45facbe2d01acd168bba864
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-12-25 17:57:50 +0800

    add compass lib.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 var es = require('event-stream'),
     gutil = require('gulp-util'),
     Buffer = require('buffer').Buffer,
-    compass = require('./lib/compass').init();
+    compass = require('./lib/compass');
 
 module.exports = function(opt){
     function compile(file, cb){
-        try {
-            //file.contents = new Buffer(coffee.compile(String(file.contents), opt));
-            file.contents = new Buffer(String(file.contents));
-        } catch (err) {
-            //var newError = formatError(file, err);
-            //return cb(newError);
-        }
-        file.path = gutil.replaceExtension(file.path, ".css");
-        //console.log(file.path);
+        var filepath = file.path;
+        compass(filepath, opt, function (error, stdout, stderr) {
+            console.log(stdout);
+            if (error !== null) {
+                gutil.log('exec error: ' + error);
+            }
+        });
         cb(null, file);
     }
 
