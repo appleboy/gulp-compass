1ddba4d5f2f7088d461030f2a68ebb654dc7b2f6
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-12-27 11:06:43 +0800

    update test
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- test/compass_test.js
+++ test/compass_test.js
@@ -1,21 +1,39 @@
 'use strict';
-var fs = require('fs');
-var gutil = require('gulp-util');
-var compass = require('../lib/compass');
-var should = require('should');
-var path = require('path');
+var fs = require('fs'),
+    compass = require('../lib/compass'),
+    should = require('should'),
+    path = require('path'),
+    grunt = require('grunt');
+
 require('mocha');
 
 describe('gulp-compass', function() {
     describe('compass()', function() {
-        it('compile scss to css', function() {
-            console.log(process.cwd() + ' ' + __dirname);
+        it('compile scss to css', function(done) {
+            var actual, expected;
             compass(path.join(__dirname, 'sass/compile.scss'), {
                 project: __dirname,
-                css: 'css2',
+                style: 'compressed',
+                css: 'css',
                 sass: 'sass'
             });
-            //done();
+
+            compass(path.join(__dirname, 'sass/simple.sass'), {
+                project: __dirname,
+                style: 'compressed',
+                css: 'css',
+                sass: 'sass'
+            });
+
+            actual = grunt.file.read(path.join(__dirname, 'css/compile.css'));
+            expected = grunt.file.read(path.join(__dirname, 'expected/compile.css'));
+            actual.should.equal(expected);
+
+            actual = grunt.file.read(path.join(__dirname, 'css/simple.css'));
+            expected = grunt.file.read(path.join(__dirname, 'expected/simple.css'));
+            actual.should.equal(expected);
+
+            done();
         });
     });
 });
