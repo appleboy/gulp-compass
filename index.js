64c358dd4d727a2dce7539d820dc9e3642527653
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-01-02 17:23:24 +0800

    fixed issue #2, support stream content.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 'use strict';
 
 var es = require('event-stream'),
+    gutil = require('gulp-util'),
+    fs = require('fs'),
     compass = require('./lib/compass');
 
 module.exports = function(opt){
     function compile(file, cb){
+        if (file.isNull()) return cb(null, file); // pass along
+        if (file.isStream()) return cb(new Error("gulp-compass: Streaming not supported"));
         var filepath = file.path;
-        compass(filepath, opt, function(code, stdout, stderr){
+        compass(filepath, opt, function(code, stdout, stderr, path){
             if (code === 127) {
                 return cb(new Error(
                     'You need to have Ruby and Compass installed ' +
                     'and in your system PATH for this task to work. '));
             }
             // excute callback
+            file.path = gutil.replaceExtension(file.path, ".css");
+            file.contents = new Buffer(fs.readFileSync(String(gutil.replaceExtension(path, '.css'))));
             cb(null, file);
         });
     }
