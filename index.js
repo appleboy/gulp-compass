0fb48faa37589413ed04997c09a94fe38ca0541c
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-01-04 12:49:51 +0800

    update index.js for jshint.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ var es = require('event-stream'),
 
 module.exports = function(opt){
     function compile(file, cb){
-        if (file.isNull()) return cb(null, file); // pass along
-        if (file.isStream()) return cb(new Error("gulp-compass: Streaming not supported"));
+        if (file.isNull()) {
+            return cb(null, file);
+        }
+        if (file.isStream()) {
+            return cb(new Error('gulp-compass: Streaming not supported'));
+        }
         var filepath = file.path;
         compass(filepath, opt, function(code, stdout, stderr, path){
             if (code === 127) {
@@ -17,7 +21,7 @@ module.exports = function(opt){
                     'and in your system PATH for this task to work. '));
             }
             // excute callback
-            file.path = gutil.replaceExtension(file.path, ".css");
+            file.path = gutil.replaceExtension(file.path, '.css');
             file.contents = new Buffer(fs.readFileSync(String(gutil.replaceExtension(path, '.css'))));
             cb(null, file);
         });
