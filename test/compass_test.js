17834a31b08c83e98ad331973c4e49288e6bd137
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-01-11 13:27:14 +0800

    add test for load config.rb file.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- test/compass_test.js
+++ test/compass_test.js
@@ -38,7 +38,7 @@ describe('gulp-compass plugin', function() {
                     throw new Error('compile scss error');
                 }
                 new_path = gutil.replaceExtension(new_path, '.css');
-                name_list.push(path.basename(new_path));
+                name_list.push(path.relative(__dirname, new_path));
                 process += 1;
             });
 
@@ -53,12 +53,24 @@ describe('gulp-compass plugin', function() {
                     throw new Error('compile sass error');
                 }
                 new_path = gutil.replaceExtension(new_path, '.css');
-                name_list.push(path.basename(new_path));
+                name_list.push(path.relative(__dirname, new_path));
+                process += 1;
+            });
+
+            compass('sass/base/compile.scss', {
+                project: __dirname,
+                config_file: path.join(__dirname, 'config.rb')
+            }, function(code, stdout, stderr, new_path){
+                if (+code !== 0) {
+                    throw new Error('compile scss error with config.rb file');
+                }
+                new_path = gutil.replaceExtension(new_path, '.css');
+                name_list.push(path.relative(__dirname, new_path));
                 process += 1;
             });
 
             timer = setInterval(function(){
-                if (process === 2) {
+                if (process === 3) {
                     clearInterval(timer);
                     done();
                 }
@@ -81,8 +93,16 @@ describe('gulp-compass plugin', function() {
             actual.should.equal(expected);
         });
 
+        it('test releate path with config.rb config', function() {
+            var actual, expected;
+
+            actual = read_file(path.join(__dirname, 'css/base/compile.css'));
+            expected = read_file(path.join(__dirname, 'expected/compile.css'));
+            actual.should.equal(expected);
+        });
+
         it('output path test array', function() {
-            var expected = ['compile.css', 'simple.css'];
+            var expected = ['sass/base/compile.css', 'sass/compile.css', 'sass/simple.css'];
             name_list.sort().should.eql(expected);
         });
     });
