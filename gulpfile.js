c3d8c18e5f8ae2a237053f2eff6c2dd0fb0ba544
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-03-14 10:41:02 +0800

    update jshin to 1.5.0 and remove default parameter ".jshintrc".
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -7,33 +7,17 @@
     var gulp = require('gulp'),
         map = require('map-stream'),
         jshint = require('gulp-jshint'),
-        fs = require('fs'),
         mocha = require('gulp-mocha'),
-
-    /**
-    * Helpers
-    */
-    readJSON = function(path) {
-        var data = fs.readFileSync(path, 'utf8');
-        try{
-            return JSON.parse(data);
-        }catch(e){
-            return JSON.parse(data.
-            replace(/(\/\/.*)/gm, '').
-            replace(/([\/][*](?:[^*]*|[*](?=[^\/]))*[*][\/])/g, ''));
-        }
-    },
 
     /**
     * Commanders
     */
     reporter = require('jshint-stylish'),
-    jshintRc = readJSON('.jshintrc'),
 
     commandJsHint = function(pattern, dontFail) {
         return function() {
             return gulp.src(pattern)
-                .pipe(jshint(jshintRc))
+                .pipe(jshint())
                 .pipe(jshint.reporter(reporter))
                 .pipe(map(function (file, cb) {
                     cb(!dontFail && !file.jshint.success, file);
@@ -44,7 +28,6 @@
     /**
     * Jshint
     */
-
     gulp.task('hint', commandJsHint(
         ['**/*.js', '!node_modules/**/*']
     ));
