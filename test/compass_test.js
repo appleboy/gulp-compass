d6242135832a4d608c6da053835d04978653be2c
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-05-08 22:01:54 +0800

    test overwrite config.rb config.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- test/compass_test.js
+++ test/compass_test.js
@@ -83,6 +83,19 @@ describe('gulp-compass plugin', function() {
         process += 1;
       });
 
+      compass(path.join(__dirname, 'sass/base/compile3.scss'), {
+        project: __dirname,
+        config_file: path.join(__dirname, 'config.rb'),
+        style: 'expanded'
+      }, function(code, stdout, stderr, new_path){
+        if (+code !== 0) {
+          throw new Error('compile scss error with config.rb file');
+        }
+        new_path = gutil.replaceExtension(new_path, '.css');
+        name_list.push(path.relative(__dirname, new_path).replace(/\\/g, '/'));
+        process += 1;
+      });
+
       compass(path.join(__dirname, 'sass/import.scss'), {
         project: __dirname,
         style: 'compressed',
@@ -231,8 +244,16 @@ describe('gulp-compass plugin', function() {
       actual.should.equal(expected);
     });
 
+    it('test config_file option and overwrite output style', function() {
+      var actual, expected;
+
+      actual = read_file(path.join(__dirname, 'css/base/compile3.css'));
+      expected = read_file(path.join(__dirname, 'expected/compile2.css'));
+      actual.should.equal(expected);
+    });
+
     it('output path test array', function() {
-      var expected = ['css/base/compile.css', 'css/base/compile2.css', 'css/compile.css', 'css/import.css', 'css/import2.css', 'css/multiple-require.css', 'css/require.css', 'css/simple.css', 'css/spriting.css'];
+      var expected = ['css/base/compile.css', 'css/base/compile2.css', 'css/base/compile3.css', 'css/compile.css', 'css/import.css', 'css/import2.css', 'css/multiple-require.css', 'css/require.css', 'css/simple.css', 'css/spriting.css'];
       name_list.sort().should.eql(expected);
     });
 
