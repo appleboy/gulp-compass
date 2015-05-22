121681a0b0012473d068aab74f076bad5c9e3208
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-05-22 09:56:27 +0800

    update coding style.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,41 @@
 'use strict';
 
-var gulp = require('gulp'),
-  $ = require('gulp-load-plugins')(),
-  del = require('del'),
-  lib = ['**/*.js', '!test/**/*', '!node_modules/**/*', '!coverage/**/*'];
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+var lib = ['**/*.js', '!test/**/*', '!node_modules/**/*', '!coverage/**/*'];
 
-
-gulp.task('coverage', ['clean'], function(){
-  return gulp.src(lib)
-    .pipe($.istanbul())
-    .pipe($.istanbul.hookRequire());
-});
-
-gulp.task('jshint', function () {
-  return gulp.src(lib)
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.jshint.reporter('fail'));
-});
-
-function mochaStream(){
+function mochaStream() {
   return gulp.src('test/*_test.js', {read: false})
     .pipe($.mocha({
       reporter: 'spec'
     }));
 }
 
-gulp.task('mocha', ['coverage'], function () {
+gulp.task('coverage', ['clean'], function() {
+  return gulp.src(lib)
+    .pipe($.istanbul())
+    .pipe($.istanbul.hookRequire());
+});
+
+gulp.task('jscs', function() {
+  return gulp.src(lib)
+    .pipe($.jscs());
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(lib)
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-reporter-jscs'))
+    .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('mocha', ['coverage'], function() {
   return mochaStream()
     .pipe($.istanbul.writeReports());
 });
 
-gulp.task('mocha:nocov', function(){
+gulp.task('mocha:nocov', function() {
   return mochaStream();
 });
 
